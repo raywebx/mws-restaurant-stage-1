05839c7aed98336ff04e082b85733d6eea9d6479
Ray Shannon <17521485+raywebx@users.noreply.github.com>
2018-05-09 21:33:32 -0500

    Stage 1 - take 2

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', 'Image of restaurant'+restaurant.name)
+
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
